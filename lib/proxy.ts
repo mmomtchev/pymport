d2190eeaa6aa222501b4663ada1d8bb54b7e43f5
Momtchil Momtchev <momtchil@momtchev.com>
2022-10-21 00:34:46 +0200

    return unique references

--- lib/proxy.ts
+++ lib/proxy.ts
@@ -1,17 +1,29 @@
 const pymportProxy = {
   get(target: any, prop: string): any {
-    if (['toJS', 'get', 'call'].includes(prop))
-      return () => target[prop]();
+    if (!target.__pymport_proxy__) target.__pymport_proxy__ = {};
+    if (target.__pymport_proxy__[prop]) return target.__pymport_proxy__[prop];
 
-    const attr = target.get(prop);
+    let r;
+    if (target[prop]) {
+      if (typeof target[prop] === 'function') {
+        r = () => target[prop]();
+      } else {
+        r = target[prop];
+      }
+    } else {
+      const attr = target.get(prop);
 
-    if (attr.callable)
-      return (...args: any[]) => {
-        const r = attr.call(...args);
-        return new Proxy(r, pymportProxy);
-      };
+      if (attr.callable)
+        r = (...args: any[]) => {
+          const r = attr.call(...args);
+          return new Proxy(r, pymportProxy);
+        };
+      else
+        r = attr;
+    }
 
-    return attr;
+    target.__pymport_proxy__[prop] = r;
+    return target.__pymport_proxy__[prop];
   }
 };
 
