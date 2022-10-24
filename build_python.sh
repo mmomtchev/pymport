ccbe874d47180b30ab60efa547a4b974be5d48ed
Momtchil Momtchev <momtchil@momtchev.com>
2022-10-24 21:22:03 +0200

    properly detect an already built Python on macOS

--- build_python.sh
+++ build_python.sh
@@ -10,26 +10,30 @@ if [ ! -r dist/Python-${VERSION}.tgz ]; then
   curl https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz --output dist/Python-${VERSION}.tgz
 fi
 
-if [ ! -d "$1" ] || [ ! -r "$1/lib/libpython3.10.so" ]; then
+case `uname` in
+  'Linux')
+    export LDFLAGS="-Wl,-z,origin -Wl,-rpath,'\$\$ORIGIN/../lib'"
+    LIBNAME="$1/lib/libpython3.10.so"
+    ;;
+  'Darwin')
+    export LDFLAGS="-Wl,-rpath,@loader_path/../lib"
+    export SSL="--with-openssl=$(brew --prefix openssl@1.1)"
+    LIBNAME="$1/lib/libpython3.10.dylib"
+    ;;
+  *)
+    echo 'Unsupported platform'
+    exit 1
+    ;;
+esac
+
+if [ ! -d "$1" ] || [ ! -r "${LIBNAME}" ]; then
   echo building in $1
   rm -rf build/Python-${VERSION}
 
   tar -C build -zxf dist/Python-${VERSION}.tgz
   (
     cd build/Python-${VERSION}
-    case `uname` in
-      'Linux')
-        export LDFLAGS="-Wl,-z,origin -Wl,-rpath,'\$\$ORIGIN/../lib'"
-        ;;
-      'Darwin')
-        export LDFLAGS="-Wl,-rpath,@loader_path/../lib"
-        export SSL="--with-openssl=$(brew --prefix openssl@1.1)"
-        ;;
-      *)
-        echo 'Unsupported platform'
-        exit 1
-        ;;
-    esac
+
     ./configure --prefix $1 --enable-shared --enable-optimizations --disable-test-modules ${SSL}
     make -j4 build_all
     make install
