94a38d9dd6246e9c44c2242d2b9f769afe3eec01
Momtchil Momtchev <momtchil@momtchev.com>
2022-11-03 10:42:37 +0100

    add a slightly optimized version

--- bench/00ndarray.bench.js
+++ bench/00ndarray.bench.js
@@ -1,5 +1,5 @@
 const b = require('benny');
-const { pymport, proxify } = require('..');
+const { pymport, proxify, PyObject } = require('..');
 
 const np = pymport('numpy');
 const proxified = proxify(np);
@@ -18,17 +18,29 @@ const js_proxified_fn = new Function('np', 'size', `
 
 const js_fn = new Function('np', 'size', `
   let b = 0;
-  const matmul = np.get('matmul');
-  const arange = np.get('arange');
-  const average = np.get('average');
   for (let i = 0; i < 100; i++) {
-    const a = matmul.call(arange.call(size * size * 2).get('reshape').call([size, size * 2]).get('T'),
-      arange.call(size * size * 2).get('reshape').call([size, size * 2]));
-    b += average.call(a).get('item').call().toJS();
+    const a = np.get('matmul').call(np.get('arange').call(size * size * 2)
+        .get('reshape').call([size, size * 2]).get('T'),
+      np.get('arange').call(size * size * 2).get('reshape').call([size, size * 2]));
+    b += np.get('average').call(a).get('item').call().toJS();
   }
   return b;
 `);
 
+const js_optimized_fn = new Function('np', 'size', 'PyObject', `
+  let b = 0;
+  const matmul = np.get('matmul');
+  const arange = np.get('arange');
+  const average = np.get('average');
+  const length = PyObject.int(size * size * 2);
+  const lengthList = PyObject.list([size, size * 2]);
+  for (let i = 0; i < 100; i++) {
+    const a = matmul.call(arange.call(length).get('reshape').call(lengthList).get('T'),
+      arange.call(length).get('reshape').call(lengthList));
+    b += average.call(a).get('item').call().toJS();
+  }
+  return b;
+`);
 
 module.exports = function (size) {
   return b.suite(
@@ -42,6 +54,9 @@ module.exports = function (size) {
     }),
     b.add('Node.js raw', () => {
       js_fn(np, size);
+    }),
+    b.add('Node.js optimized', () => {
+      js_optimized_fn(np, size, PyObject);
     }),
     b.cycle(),
     b.complete()
