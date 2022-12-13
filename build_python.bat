6b2794b082531bb75c5baec089b4c604b0e0ff26
Momtchil Momtchev <momtchil@momtchev.com>
2022-12-13 15:58:42 +0100

    fix windows build

--- build_python.bat
+++ build_python.bat
@@ -4,11 +4,11 @@ set mypath=%~dp0
 cd %mypath:~0,-1%
 
 if not exist dist mkdir dist
-if not exist dist/Python-%VERSION%.tgz (
+if not exist dist\Python-%VERSION%.tgz (
   curl https://www.python.org/ftp/python/%VERSION%/Python-%VERSION%.tgz --output dist\Python-%VERSION%.tgz
 )
 
-if not exist "%1/python311.lib" (
+if not exist "%1\python311.lib" (
   echo building in %1
   rd /q /s build\Python-%VERSION%
 
@@ -16,5 +16,6 @@ if not exist "%1/python311.lib" (
   build\Python-%VERSION%\PCBuild\build.bat
   (robocopy build\Python-%VERSION%\PCBuild\amd64 %1 /MIR) ^& if %ERRORLEVEL% leq 1 set ERRORLEVEL = 0
   (robocopy build\Python-%VERSION%\Lib %1\lib /MIR) ^& if %ERRORLEVEL% leq 1 set ERRORLEVEL = 0
+  set PYTHONHOME=%1
   %1\python -m ensurepip
 )
