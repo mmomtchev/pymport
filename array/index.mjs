de22f1ddb88ab0ce565fe5e92708a0713e92d2ea
Momtchil Momtchev <momtchil@momtchev.com>
2022-12-13 13:52:06 +0100

    named ES6 exports

---
+++ array/index.mjs
@@ -0,0 +1,6 @@
+import cjs from './index.js';
+
+export const getJSType = cjs.getJSType;
+export const getPythonType = cjs.getPythonType;
+export const toTypedArray = cjs.toTypedArray;
+export const toPythonArray = cjs.getPythonType;
---
+++ proxified/index.mjs
@@ -0,0 +1,7 @@
+import main from './index.js';
+
+export const pymport = main.pymport;
+export const proxify = main.proxify;
+export const PyObject = main.PyObject;
+export const pyval = main.pyval;
+export const version = main.version;
