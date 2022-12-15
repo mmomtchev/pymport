22dabd8834c52e3a18712663ef039199af55f13b
Momtchil Momtchev <momtchil@momtchev.com>
2022-12-15 17:12:55 +0100

    link statically OpenSSL on macOS (#39)
    * static openssl build on macos
    
    * speed up the builds

--- build_python.sh
+++ build_python.sh
@@ -10,37 +10,43 @@ if [ ! -r dist/Python-${VERSION}.tgz ]; then
   curl https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz --output dist/Python-${VERSION}.tgz
 fi
 
-case `uname` in
-  'Linux')
-    export LDFLAGS="-Wl,-z,origin -Wl,-rpath,'\$\$ORIGIN/../lib'"
-    export CFLAGS=""
-    LIBNAME="$1/lib/libpython3.11.so"
-    export ZLIB_LIBS="-lz -ldl"
-    export PY_UNSUPPORTED_OPENSSL_BUILD=static
-    ;;
-  'Darwin')
-    export LDFLAGS="-Wl,-rpath,@loader_path/../lib"
-    export SSL="--with-openssl=$(brew --prefix openssl@1.1)"
-    LIBNAME="$1/lib/libpython3.11.dylib"
-    ;;
-  *)
-    echo 'Unsupported platform for the builtin Python interpreter'
-    exit 1
-    ;;
-esac
-
 if [ ! -d "$1" ] || [ ! -r "${LIBNAME}" ]; then
   echo building in $1
   rm -rf build/Python-${VERSION}
+  rm -rf build/openssl
 
   tar -C build -zxf dist/Python-${VERSION}.tgz
   (
     cd build/Python-${VERSION}
-    sed -i 's/ffi_lib = None/ffi_lib=":libffi_pic.a"/g' setup.py
 
-    ./configure --prefix $1 --enable-shared --enable-optimizations ${SSL}
+    export PY_UNSUPPORTED_OPENSSL_BUILD=static
+    case `uname` in
+      'Linux')
+        export LDFLAGS="-Wl,-z,origin -Wl,-rpath,'\$\$ORIGIN/../lib'"
+        export CFLAGS=""
+        LIBNAME="$1/lib/libpython3.11.so"
+        export ZLIB_LIBS="-lz -ldl"
+        patch < ../../patches/python-3.11-setup.py.patch
+        ;;
+      'Darwin')
+        export LDFLAGS="-Wl,-rpath,@loader_path/../lib"
+        mkdir -p ../openssl/lib
+        mkdir -p ../openssl/include
+        cp $(brew --prefix openssl@1.1)/lib/*.a ../openssl/lib
+        cp -r $(brew --prefix openssl@1.1)/include/openssl ../openssl/include
+        export SSL="--with-openssl=$(pwd)/../openssl"
+        LIBNAME="$1/lib/libpython3.11.dylib"
+        patch < ../../patches/python-3.11-configure.patch
+        ;;
+      *)
+        echo 'Unsupported platform for the builtin Python interpreter'
+        exit 1
+        ;;
+    esac
+
+    ./configure --prefix $1 --enable-shared --enable-optimizations --without-system-ffi ${SSL}
     make -j4 build_all
-    make install
+    make -j4 install
   )
   rm -f $1/python
   ln -s bin/python3 $1/python
