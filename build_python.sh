37c822aba394a25868b912139c4c80ac9bd6df20
Momtchil Momtchev <momtchil@momtchev.com>
2022-12-15 14:25:40 +0100

    static openssl build on macos

--- build_python.sh
+++ build_python.sh
@@ -10,25 +10,6 @@ if [ ! -r dist/Python-${VERSION}.tgz ]; then
   curl https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz --output dist/Python-${VERSION}.tgz
 fi
 
-case `uname` in
-  'Linux')
-    export LDFLAGS="-Wl,-z,origin -Wl,-rpath,'\$\$ORIGIN/../lib'"
-    export CFLAGS=""
-    LIBNAME="$1/lib/libpython3.11.so"
-    export ZLIB_LIBS="-lz -ldl"
-    export PY_UNSUPPORTED_OPENSSL_BUILD=static
-    ;;
-  'Darwin')
-    export LDFLAGS="-Wl,-rpath,@loader_path/../lib"
-    export SSL="--with-openssl=$(brew --prefix openssl@1.1)"
-    LIBNAME="$1/lib/libpython3.11.dylib"
-    ;;
-  *)
-    echo 'Unsupported platform for the builtin Python interpreter'
-    exit 1
-    ;;
-esac
-
 if [ ! -d "$1" ] || [ ! -r "${LIBNAME}" ]; then
   echo building in $1
   rm -rf build/Python-${VERSION}
@@ -36,9 +17,31 @@ if [ ! -d "$1" ] || [ ! -r "${LIBNAME}" ]; then
   tar -C build -zxf dist/Python-${VERSION}.tgz
   (
     cd build/Python-${VERSION}
-    sed -i 's/ffi_lib = None/ffi_lib=":libffi_pic.a"/g' setup.py
 
-    ./configure --prefix $1 --enable-shared --enable-optimizations ${SSL}
+    export PY_UNSUPPORTED_OPENSSL_BUILD=static
+    case `uname` in
+      'Linux')
+        export LDFLAGS="-Wl,-z,origin -Wl,-rpath,'\$\$ORIGIN/../lib'"
+        export CFLAGS=""
+        LIBNAME="$1/lib/libpython3.11.so"
+        export ZLIB_LIBS="-lz -ldl"
+        patch < ../../patches/python-3.11-setup.py.patch
+        ;;
+      'Darwin')
+        export LDFLAGS="-Wl,-rpath,@loader_path/../lib"
+        cp $(brew --prefix openssl@1.1)/lib/*.a lib
+        cp -r $(brew --prefix openssl@1.1)/include/openssl include
+        export SSL="--with-openssl=$(pwd)"
+        LIBNAME="$1/lib/libpython3.11.dylib"
+        patch < ../../patches/python-3.11-configure.patch
+        ;;
+      *)
+        echo 'Unsupported platform for the builtin Python interpreter'
+        exit 1
+        ;;
+    esac
+
+    ./configure --prefix $1 --enable-shared --enable-optimizations --without-system-ffi ${SSL}
     make -j4 build_all
     make install
   )
