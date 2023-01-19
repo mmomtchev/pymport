6384504e19dec1d75469262b996ad1b066b68711
Momtchil Momtchev <momtchil@momtchev.com>
2023-01-19 20:58:59 +0100

    more thorough stress test (#72)
    * more thorough stress test
    
    * full throttle

--- test/worker-stress-test.js
+++ test/worker-stress-test.js
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { Worker } = require('worker_threads');
 const path = require('path');
+const Queue = require('async-await-queue');
+
+const queue = new Queue(24, 0);
 
 // Make sure the dynamic library is loaded from the main thread
 // https://github.com/mmomtchev/pymport/issues/69
@@ -26,10 +29,20 @@ async function main() {
   console.log('start');
   // eslint-disable-next-line no-constant-condition
   while (true) {
-    const r = await spawnWorker('lambda x: x + 4.2');
-    if (r !== 8.4) throw new Error('r=' + r);
-    i++;
-    if (i % 10 == 0) console.log('deca', i / 10);
+    const me = Symbol();
+    queue.wait(me)
+      .then(() => spawnWorker('lambda x: x + 4.2'))
+      .then((r) => {
+        if (Math.abs(r - 4.2 * 102) > 0.1) throw new Error('r=' + r);
+        i++;
+        if (i % 10 == 0) console.log('deca', i / 10);
+      })
+      .finally(() => queue.end(me));
+    const { waiting } = queue.stat();
+    if (waiting > 100) {
+      const throttle = Symbol();
+      await queue.wait(throttle, -1).finally(() => queue.end(throttle));
+    }
   }
 }
 
--- test/worker_thread.js
+++ test/worker_thread.js
@@ -6,6 +6,10 @@ if (!parentPort) throw new Error('This worker must be spawned from another test'
 
 const script = workerData;
 const fn = pyval(script);
-fn.callAsync(4.2).then((result) => {
-  parentPort.postMessage(result.toJS());
-});
+
+let q = fn.callAsync(4.2);
+for (let i = 0; i < 100; i++) {
+  q = q.then((r) => fn.callAsync(r));
+}
+
+q.then((r) => parentPort.postMessage(r.toJS()));
