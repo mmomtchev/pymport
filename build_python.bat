f01dfbc65aff0659778df0f3cce5c18994b58835
Momtchil Momtchev <momtchil@momtchev.com>
2024-05-27 21:29:52 +0200

    fix the windows path

--- build_python.bat
+++ build_python.bat
@@ -4,5 +4,5 @@ cd %mypath:~0,-1%
 set PYTHON_DIST=%mypath:~0,-1%\dist
 set PYTHON_BUILD=%mypath:~0,-1%\build
 set PYTHON_VERSION=%BUILTIN_PYTHON_VERSION%
-cd deps\static_portable_python
+cd deps\static-portable-python
 build_python.bat %1
